76c4106df3914136d9ab0a77d42397d94b3b95fb
Jannis Spiekermann <janniss@hotmail.de>
2021-01-02 13:29:48 +0100

    Implement SwissDialectDataset class

---
+++ scripts/dataset.py
@@ -0,0 +1,33 @@
+import csv
+import torch
+
+from torch.utils.data import Dataset
+
+
+class SwissDialectDataset(Dataset):
+    def __init__(self, vec_file, txt_file):
+        self.dialect_map = {"LU": 1, "BE": 2, "ZH": 3, "BS": 4}
+
+        self.ivectors = self.load_ivectors(vec_file)
+        self.labels = self.load_labels(txt_file)
+        self.n_samples = self.ivectors.shape[0]
+
+    def load_ivectors(self, vec_file):
+        with open(vec_file) as in_file:
+            reader = csv.reader(in_file, delimiter=" ")
+            ivectors = [[float(num) for num in ivec] for ivec in reader]
+
+        return torch.FloatTensor(ivectors)
+
+    def load_labels(self, txt_file):
+        with open(txt_file) as in_file:
+            reader = csv.reader(in_file, delimiter="\t")
+            numeric_labels = [self.dialect_map[label] for _utterance, label in reader]
+
+        return torch.ByteTensor(numeric_labels)
+
+    def __getitem__(self, index):
+        return self.ivectors[index], self.labels[index]
+
+    def __len__(self):
+        return self.n_samples
