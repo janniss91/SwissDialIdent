1cf15777717a1b0e77310b0d2cd1b2c5f0d81078
Jannis Spiekermann <janniss@hotmail.de>
2021-01-04 09:52:32 +0100

    Fix Class Tensor

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -6,12 +6,12 @@ from torch.utils.data import Dataset
 
 class SwissDialectDataset(Dataset):
     def __init__(self, vec_file, txt_file):
-        self.dialect_map = {"LU": 1, "BE": 2, "ZH": 3, "BS": 4}
+        self.dialect_map = {"LU": 0, "BE": 1, "ZH": 2, "BS": 3}
 
         self.ivectors = self.load_ivectors(vec_file)
         self.labels = self.load_labels(txt_file)
         self.n_samples = self.ivectors.shape[0]
-        self.n_params = self.ivectors.shape[1]
+        self.n_features = self.ivectors.shape[1]
         self.n_classes = len(self.dialect_map)
 
     def load_ivectors(self, vec_file):
@@ -26,7 +26,7 @@ class SwissDialectDataset(Dataset):
             reader = csv.reader(in_file, delimiter="\t")
             numeric_labels = [self.dialect_map[label] for _utterance, label in reader]
 
-        return torch.ByteTensor(numeric_labels)
+        return torch.LongTensor(numeric_labels)
 
     def __getitem__(self, index):
         return self.ivectors[index], self.labels[index]
