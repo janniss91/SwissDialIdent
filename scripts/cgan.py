a10ee76a6d0cb6d87d2d810498e2b0ab76fc8859
Jannis Spiekermann <ssprmann@tesniere.sfs.uni-tuebingen.de>
2021-02-07 17:25:18 +0100

    Make all parts of CGAN work

--- scripts/cgan.py
+++ scripts/cgan.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,19 +10,6 @@ from dataset import load_labels
 from dataset import SwissDialectDataset
 
 
-TRAIN_VEC_FILE = "data/train.vec"
-TRAIN_TXT_FILE = "data/train.txt"
-
-train_ivectors = load_ivectors(TRAIN_VEC_FILE)
-train_labels = load_labels(TRAIN_TXT_FILE)
-
-dataset = SwissDialectDataset(train_ivectors, train_labels)
-
-batch_size = 32
-data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-
-
 class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
@@ -30,7 +18,7 @@ class Discriminator(nn.Module):
 
         # TODO: Maybe remove one of the linear layers (the one with 128 first).
         self.model = nn.Sequential(
-            nn.Linear(404, 512),  # 404 is num features + embeddings
+            nn.Linear(410, 512),  # 410 is num features + embeddings
             nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.3),
             nn.Linear(512, 256),
@@ -44,8 +32,6 @@ class Discriminator(nn.Module):
         )
 
     def forward(self, x, labels):
-        # TODO: The view can probably be omitted (because it does nothing).
-        x = x.view(x.size(0), 400)  ## 400 is the number of features in i-vectors.
         c = self.label_emb(labels)
         x = torch.cat([x, c], 1)
         out = self.model(x)
@@ -59,7 +45,7 @@ class Generator(nn.Module):
         self.label_emb = nn.Embedding(4, 10)  # The 4 is for the four classes.
 
         self.model = nn.Sequential(
-            nn.Linear(104, 128),  # 104 is 100 random datapoints + embeddings
+            nn.Linear(110, 128),  # 110 is 100 random datapoints + embeddings
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(128, 256),
             nn.LeakyReLU(0.2, inplace=True),
@@ -70,16 +56,14 @@ class Generator(nn.Module):
         )
 
     def forward(self, z, labels):
-        # TODO: The view can probably be omitted (because it does nothing).
-        z = z.view(z.size(0), 100)
         c = self.label_emb(labels)
         x = torch.cat([z, c], 1)
         out = self.model(x)
-        return out
+        return out.squeeze()
 
 
 def generator_train_step(batch_size, discriminator, generator, gen_optimizer, criterion):
-    g_optimizer.zero_grad()
+    gen_optimizer.zero_grad()
     z = Variable(torch.randn(batch_size, 100))
     fake_labels = Variable(torch.LongTensor(np.random.randint(0, 4, batch_size)))
     fake_ivectors = generator(z, fake_labels)
@@ -87,13 +71,13 @@ def generator_train_step(batch_size, discriminator, generator, gen_optimizer, cr
     gen_loss = criterion(validity, Variable(torch.ones(batch_size)))
     gen_loss.backward()
     gen_optimizer.step()
-    return g_loss.item()
+    return gen_loss.item()
 
 
 def discriminator_train_step(
     batch_size, discriminator, generator, discr_optimizer, criterion, real_ivectors, labels
 ):
-    d_optimizer.zero_grad()
+    discr_optimizer.zero_grad()
 
     # Train with real ivectors.
     real_validity = discriminator(real_ivectors, labels)
@@ -111,48 +95,7 @@ def discriminator_train_step(
     discr_loss = real_loss + fake_loss
     discr_loss.backward()
     discr_optimizer.step()
-    return d_loss.item()
-
-
-writer = SummaryWriter()
-
-generator = Generator()
-discriminator = Discriminator()
-
-criterion = nn.BCELoss()
-discr_optimizer = torch.optim.Adam(discriminator.parameters(), lr=1e-4)
-gen_optimizer = torch.optim.Adam(generator.parameters(), lr=1e-4)
-
-num_epochs = 100
-n_critic = 5  # TODO: variable is not used?
-for epoch in range(num_epochs):
-    print("Epoch {}...".format(epoch + 1), end=" ")
-    for i, (ivectors, labels) in enumerate(data_loader):
-
-        step = epoch * len(data_loader) + i + 1
-        real_ivectors = Variable(ivectors)
-        labels = Variable(labels)
-        generator.train()
-
-        d_loss = discriminator_train_step(
-            len(real_ivectors),
-            discriminator,
-            generator,
-            discr_optimizer,
-            criterion,
-            real_ivectors,
-            labels,
-        )
-
-        g_loss = generator_train_step(
-            batch_size, discriminator, generator, gen_optimizer, criterion
-        )
-
-        writer.add_scalars("scalars", {"g_loss": g_loss, "d_loss": d_loss}, step)
-
-    print("Done!")
-
-    torch.save(generator.state_dict(), 'generator_state.pt')
+    return discr_loss.item()
 
 
 def generate_ivector(generator, dialect):
@@ -162,9 +105,102 @@ def generate_ivector(generator, dialect):
     return ivector
 
 
-# Model loading process:
+if __name__ == "__main__":
 
-# model = Generator(*args, **kwargs)
-# model.load_state_dict(torch.load(PATH))
-# model.eval()
-# generate_digit(model, dialect)
+    parser = argparse.ArgumentParser()
+
+    # parser.add_argument("config_file", help="A configuration file that specifies training parameters.")
+    parser.add_argument(
+        "-s",
+        "--single_model",
+        choices=["LogisticRegression", "SVM"],
+        help="Train a single model.",
+    )
+    parser.add_argument(
+        "-f",
+        "--final_model",
+        choices=["LogisticRegression", "SVM"],
+        help="Train a single model with the entire dataset",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Print training and testing metrics.",
+    )
+
+    args = parser.parse_args()
+
+    TRAIN_VEC_FILE = "data/train.vec"
+    TRAIN_TXT_FILE = "data/train.txt"
+
+    train_ivectors = load_ivectors(TRAIN_VEC_FILE)
+    train_labels = load_labels(TRAIN_TXT_FILE)
+
+    dataset = SwissDialectDataset(train_ivectors, train_labels)
+
+    batch_size = 32
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    writer = SummaryWriter()
+
+    generator = Generator()
+    discriminator = Discriminator()
+
+    criterion = nn.BCELoss()
+    discr_optimizer = torch.optim.Adam(discriminator.parameters(), lr=1e-4)
+    gen_optimizer = torch.optim.Adam(generator.parameters(), lr=1e-4)
+
+    num_epochs = 100
+    for epoch in range(num_epochs):
+        print("Epoch {}...".format(epoch + 1), end=" ")
+        for i, (ivectors, labels) in enumerate(data_loader):
+
+            step = epoch * len(data_loader) + i + 1
+            real_ivectors = Variable(ivectors)
+            labels = Variable(labels)
+            generator.train()
+
+            d_loss = discriminator_train_step(
+                len(real_ivectors),
+                discriminator,
+                generator,
+                discr_optimizer,
+                criterion,
+                real_ivectors,
+                labels,
+            )
+
+            g_loss = generator_train_step(
+                batch_size, discriminator, generator, gen_optimizer, criterion
+            )
+
+            writer.add_scalars("scalars", {"g_loss": g_loss, "d_loss": d_loss}, step)
+
+        print("Done!")
+
+        torch.save(generator.state_dict(), 'generator_state.pt')
+
+
+    dialects = [[x]* 3000 for x in (0,1,2,3)]
+
+    flat_list = []
+    for sublist in dialects:
+        for item in sublist:
+            flat_list.append(item)
+
+    dialects = flat_list
+
+    with open("generated_utterances.csv", "w") as gen_file, open("gen"):
+        ivec_writer = csv.writer(gen_file)
+        for dialect in dialects:
+            gen_ivec = generate_ivector(generator, dialect)
+            ivec_writer.writerow(gen_ivec.tolist())
+
+
+    # Model loading process:
+
+    # model = Generator(*args, **kwargs)
+    # model.load_state_dict(torch.load(PATH))
+    # model.eval()
+    # generate_digit(model, dialect)
